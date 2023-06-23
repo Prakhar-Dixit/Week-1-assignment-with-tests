a7d9dce2a8fe39349fcdd42a05203dc98f1f8498
Prakhar-Dixit <dixitprakhar11@gmail.com>
2023-06-23 19:33:47 +0530

    assignment updated

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -7,6 +7,15 @@ Try running it for
 Hint - use Date class exposed in JS
 */
 
-function calculateTime(n) {
-    return 0.01;
+// function calculateTime(n) {
+//     return 0.01;
+// }
+function sumAll(n){
+    var startTime = new Date().getTime();
+    var sum = 0;
+for(var i = 1; i<=n; i++){
+    sum += i;
+}
+    var endTime = new Date().getTime();
+     return (endTime-startTime)/1000;
 }
